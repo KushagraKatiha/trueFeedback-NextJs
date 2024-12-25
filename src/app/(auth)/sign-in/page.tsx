2208659f052cec41745b89a065e2096504f0ced5
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-12-26 01:09:12 +0530

    configure next-auth credential login

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+function page() {
+  return (
+    <button className='bg-blue-500 text-white p-2 rounded-md'>Sign In</button>
+  )
+}
+
+export default page
